a4929fb701f6859ab9c9571991061e83d55a4251
maxpanki <maxrepan@gmail.com>
2019-05-08 13:27:48 +0200

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Maksim Pansevich on 5/8/19.
+//  Copyright © 2019 Maksim Pansevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
